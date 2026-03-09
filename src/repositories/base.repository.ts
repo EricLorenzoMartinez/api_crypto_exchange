9f58d7fa6874d18932013fb506bab122fd40b075
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-03-09 22:00:28 +0100

    base repo created with necessary methods to start crud creation

---
+++ src/repositories/base.repository.ts
@@ -0,0 +1,35 @@
+import { Document, Model, Types } from 'mongoose';
+import logger from '../config/logger';
+
+/**
+ * Common base repository for all entities, providing basic CRUD operations.
+ */
+export abstract class BaseRepository<T, TModel extends Document> {
+    // Variables
+    protected model: Model<TModel>;
+    protected entityName: string;
+
+    /**
+     * Constructor
+     * @param model 
+     * @param entityName 
+     */
+    constructor(model: Model<TModel>, entityName: string) {
+        this.model = model;
+        this.entityName = entityName;
+    }
+
+    /**
+     * Transform Mongoose doc to plain object with 'id' instead of '_id'
+     */
+    protected transformId(doc: TModel): T {
+        const startObj = doc.toObject() as Record<string, unknown> & { _id: Types.ObjectId };
+
+        const { _id, ...rest } = startObj;
+
+        return {
+            id: _id.toString(),
+            ...rest,
+        } as unknown as T;
+    }
+};
