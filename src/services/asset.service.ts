9b3d334e7f1888105ae2894bf3bb0dd744e181d5
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-03-09 22:48:56 +0100

    asset function getbyid done

--- src/services/asset.service.ts
+++ src/services/asset.service.ts
@@ -1,6 +1,8 @@
 import { AssetRepository } from '../repositories/asset.repository';
 import { IAsset } from '../interfaces/asset.interface';
 import logger from '../config/logger';
+import { AppError } from '../utils/application.error';
+import { httpStatus } from '../config/httpStatusCodes';
 
 export class AssetService {
     private readonly repository: AssetRepository;
@@ -15,5 +17,15 @@ export class AssetService {
     }): Promise<IAsset[]> {
         logger.debug(`Service: Getting all assets with pagination: ${JSON.stringify(pagination)}`);
         return this.repository.getAll({}, pagination);
+    }
+
+    async getAssetById(id: string): Promise<IAsset | null> {
+        logger.debug(`Service: Getting asset by id: ${id}`);
+        const asset = await this.repository.getById(id);
+        if (!asset) {
+            logger.warn(`Asset with id ${id} not found`);
+            throw new AppError(`Asset not found`, httpStatus.NOT_FOUND, { id });
+        }
+        return asset;
     }
 }
