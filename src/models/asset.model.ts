fbfe17cf9786b1e25272ecf44c137bf1f21abbed
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-03-09 20:11:48 +0100

    asset interface and model done with required labels and validations

---
+++ src/models/asset.model.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { IAsset } from '../interfaces/asset.interface';
+
+const assetSchema = new mongoose.Schema(
+    {
+        symbol: { type: String, required: true },
+        name: { type: String, required: true },
+        coincapId: { type: String, required: true, unique: true },
+        lastPriceUsd: { type: Number, required: false },
+        lastPriceAt: { type: Date, required: false },
+    }
+);
+
+export const AssetModel = mongoose.model<IAsset>('Asset', assetSchema);
