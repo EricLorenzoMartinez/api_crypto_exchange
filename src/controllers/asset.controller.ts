6e8610754a16e2fd02df9635c9b15d78e9454e77
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-03-10 17:01:16 +0100

    method asset by id for controller done

--- src/controllers/asset.controller.ts
+++ src/controllers/asset.controller.ts
@@ -5,6 +5,7 @@ import { AssetService } from '../services/asset.service';
 import { toAssetResponse, toCreateAssetInput } from '../mappers/asset.mapper';
 import { CreateAssetDto } from '../dtos/asset.dto';
 import { AppError } from '../utils/application.error';
+import { app } from '../app';
 
 export class AssetController {
     private assetService: AssetService;
@@ -31,8 +32,35 @@ export class AssetController {
             res.send(response);
         } catch (error) {
             let appError = error as unknown;
+            logger.debug('Controller: Error fetching assets');
             if (!(appError instanceof AppError)) {
-                appError = new AppError('Error occurred while retrieving assets', httpStatus.INTERNAL_SERVER_ERROR);
+                appError = new AppError('Error occurred while retrieving assets', httpStatus.INTERNAL_SERVER_ERROR, {originalError: appError});
+            }
+            next(appError);
+        }
+    }
+
+    async getAssetById(req: Request, res: Response, next: NextFunction): Promise<void> {
+        const assetId = req.params.id;
+        logger.debug(`Controller: Received request to get asset by ID: ${assetId}`);
+        try {
+            const asset = await this.assetService.getAssetById(assetId);
+            if (!asset) {
+                res.status(httpStatus.NOT_FOUND).send({ error: 'Asset not found' });
+                return;
+            }
+            const data = toAssetResponse(asset);
+
+            const response = {
+                message: 'Asset retrieved successfully',
+                data,
+            };
+            res.send(response);
+        } catch (error) {
+            let appError = error as unknown;
+            logger.debug(`Controller: Error fetching asset by id: ${assetId}`);
+            if (!(appError instanceof AppError)) {
+                appError = new AppError('Error occurred while retrieving asset', httpStatus.INTERNAL_SERVER_ERROR, {id: req.params.id, originalError: appError});
             }
             next(appError);
         }
