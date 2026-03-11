bc57640b429c41a199adbbeba4dfd78738597b2e
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-03-11 19:41:36 +0100

    portfolio routes created and added to base routes

--- src/routes/base.routes.ts
+++ src/routes/base.routes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { assetRouter } from './asset.routes';
 import { transactionRouter } from './transaction.routes';
 import { authRouter } from './auth.routes';
+import { portfolioRouter } from './portfolio.routes';
 
 export const baseRouter = Router();
 
@@ -10,3 +11,4 @@ baseRouter.use(express.json());
 baseRouter.use('/assets', assetRouter);
 baseRouter.use('/transactions', transactionRouter);
 baseRouter.use('/auth', authRouter);
+baseRouter.use('/portfolio', portfolioRouter)
